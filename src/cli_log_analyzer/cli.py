18c68b63b8c8b7628db3e7c19a9c2e07efd6808c
Dave Moran <dave.moran118@gmail.com>
2026-02-16 13:22:30 -0500

    CLILA-3: test out asyncio

--- src/cli_log_analyzer/cli.py
+++ src/cli_log_analyzer/cli.py
@@ -1,11 +1,19 @@
 """
 This is the main entry point for the CLI Log Analyzer project.
+
+- Write experimental script: basic coroutine with `await asyncio.sleep()`
+- Write experimental script: `asyncio.gather()` reading multiple files concurrently
+- Write experimental script: `async for` with an async generator
 """
 
+import asyncio
 
-def main():
-    pass
+
+async def main():
+    print("Hello before sleep")
+    await asyncio.sleep(5)
+    print("Hello after sleep")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
