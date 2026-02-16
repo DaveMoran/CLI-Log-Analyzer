d2f75483b7f162e17f4c4cd0af4b36c1f26d67c6
Dave Moran <dave.moran118@gmail.com>
2026-02-16 13:28:14 -0500

    CLILA-3: add three separate definitions for files

--- src/cli_log_analyzer/cli.py
+++ src/cli_log_analyzer/cli.py
@@ -11,6 +11,8 @@ import asyncio
 
 async def main():
     await exercise_1()
+    results = await asyncio.gather(exercise_2_file1(), exercise_2_file2(), exercise_2_file3())
+    print(results)
 
 
 async def exercise_1():
@@ -18,6 +20,27 @@ async def exercise_1():
     print("Starting the CLI Log Analyzer...")
     await asyncio.sleep(1)  # Simulate some startup delay
     print("Initialization complete.")
+
+
+async def exercise_2_file1():
+    # Step 2: `asyncio.gather()` reading multiple files concurrently
+    with open("./sample_logs/access.log", "r") as f:
+        data = f.read()
+    return data
+
+
+async def exercise_2_file2():
+    # Step 2: `asyncio.gather()` reading multiple files concurrently
+    with open("./sample_logs/error.log", "r") as f:
+        data = f.read()
+    return data
+
+
+async def exercise_2_file3():
+    # Step 2: `asyncio.gather()` reading multiple files concurrently
+    with open("./sample_logs/app.log", "r") as f:
+        data = f.read()
+    return data
 
 
 if __name__ == "__main__":
