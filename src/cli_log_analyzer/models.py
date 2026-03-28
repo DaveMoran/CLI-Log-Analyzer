451b106fca76d738741706fe027624695f19d3a7
Dave Moran <dave.moran118@gmail.com>
2026-03-28 12:09:50 -0400

    feat: add skeleton for logentry

--- src/cli_log_analyzer/models.py
+++ src/cli_log_analyzer/models.py
@@ -1,3 +1,14 @@
 """
 Different datamodels used in the CLI Log Analyzer project.
 """
+
+from dateutil import parser
+
+
+class LogEntry:
+    def __init__(self, timestamp, level, source, message, metadata):
+        self.timestamp = parser.parse(timestamp)
+        self.level = level
+        self.source = source
+        self.message = message
+        self.metadata = metadata
