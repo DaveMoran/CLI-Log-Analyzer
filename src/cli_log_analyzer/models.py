5739b8dbb5fc16a9e65ddae291e0368b9e485631
Dave Moran <dave.moran118@gmail.com>
2026-03-28 12:18:31 -0400

    chore: add docstring to LogEntry

--- src/cli_log_analyzer/models.py
+++ src/cli_log_analyzer/models.py
@@ -6,6 +6,19 @@ from dateutil import parser
 
 
 class LogEntry:
+    """
+    Normalizes a log entry into standardized fields so that they are easier
+    to filter and search through
+
+    Parameters:
+    -----------
+      timestamp: the date/time that the log occured
+      level:     the level of severity of the log entry
+      source:    file and line number that the log originated from
+      message:   text that was received with the log
+      metadata:  additional information to be saved along each entry
+    """
+
     def __init__(self, timestamp, level, source, message, metadata):
         self.timestamp = parser.parse(timestamp)
         self.level = level
